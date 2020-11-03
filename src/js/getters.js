d9be05a2ac3ec608e7012941cfeb6c14389bc3d1
vapaperno@sberbank.ru (JAICP) <vapaperno@sberbank.ru>
2020-11-03 15:14:23 +0000

    Edited files: /src/js/getters.js /src/js/reply.js /src/sc/addNote.sc /src/sc/deleteNote.sc

--- src/js/getters.js
+++ src/js/getters.js
@@ -39,13 +39,23 @@ if (request &&
     return null;
 }
 
-function get_item_selector(request){
+function get_items(request){
 if (request &&
         request.payload &&
         request.payload.meta &&
         request.payload.meta.current_app &&
-        request.payload.meta.current_app.state){
-        return request.payload.meta.current_app.state.item_selector;
+        request.payload.meta.current_app.state &&
+        request.payload.meta.current_app.state.item_selector){
+        return request.payload.meta.current_app.state.item_selector.items;
+    }
+    return null;
+}
+
+function get_id_by_selected_item(request){
+    var items = get_items(request);
+    var selected_item = get_selected_item(request);
+    if (selected_item && items) {
+        return items[selected_item.index].id
     }
     return null;
 }
--- src/sc/deleteNote.sc
+++ src/sc/deleteNote.sc
@@ -7,4 +7,7 @@ theme: /
             
         
         script:
-            var selected_item = get_selected_item($context);
+            var item_id = get_id_by_selected_item(get_request($context));
+            if (item_id !== null){
+                deleteNote(item_id,
+            }
